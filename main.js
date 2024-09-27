58b4a5ab4470a31ef39c427fe81a7db2afe9f7e8
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-09-27 12:20:46 -0300

    sadsadsadasdasdasdasdasdasdasdsadsadasdasdasdsadsasdsasdsasdsasdsasdsadsasdsasdsaddsasdsadsasdsadsadsadsasdsadsadsadsadsadsasd nao vo

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function areaQuadrado (lado){
     return lado**2;
 }
-console.log(areaQuadrado(9,9,9,9));
+console.log(areaQuadrado(2,2));
 let média=function(num1,num2,num3,num4){
-    return num1,num2,num3/num4;
+    return (num1,num2,num3)/num3;
 }
-console.log(média(9,9,9,9));
+console.log(média(1,2,3));
+let nome=parseInt(prmpt(1+2));
+document.write(1+2)
