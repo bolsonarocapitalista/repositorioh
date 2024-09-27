2dbebbe7aa780375cb7b6356ce8724babdd2eb39
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-09-27 11:59:39 -0300

    r

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function soma (num1,num2){
-    return num1*num2
+function areaQuadrado (lado){
+    return lado**2;
 }
-console.log(soma(9,7));
-
-
+console.log(areaQuadrado(9,9,9,9));
+let média=function(num1,num2,num3,num4){
+    return num1,num2,num3,num4
+}
+console.log()
